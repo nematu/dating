90617c9401ec016a63bb3d8b064ce7683120889a
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-02-26 19:45:38 -0500

    updated data-layer

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,8 +1,18 @@
 <?php
+/** Data-layer for Dating site
+ * Functions to collect data
+ *
+ * Nematullah Ayaz
+ * 02/25/2021
+ */
+
+/** Function to get to return Gender selection */
 function getGenders()
 {
-    return array('Male', 'Female');
+    return array('Male', 'Female','Others');
 }
+
+/** Function to get to return States  */
 function getStates()
 {
     return array('Alabama','Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
@@ -14,11 +24,15 @@ function getStates()
         'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia',
         'Washington', 'West Virginia', 'Wisconsin', 'Wyoming');
 }
+
+/** Function to get to return Indoor Interests */
 function getIndoor()
 {
     return array('Tv', 'Movies', 'Cooking', 'Board Games', 'Puzzles', 'Reading', 'Playing cards', 'Video games');
 }
 
+
+/** Function to get to return Outdoor Interests */
 function getOutdoor()
 {
     return array('Hiking', 'Biking', 'Swimming', 'Collecting', 'Walking', 'Climbing');
